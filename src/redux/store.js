41274a3ea29eb30e10ab9676cbd05792f01d6eb8
kurvahari <harikrishna62815755@gmail.com>
2022-08-27 16:23:15 +0530

    passing the store to app

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { createStore,applyMiddleware } from "redux";
 import thunk from "redux-thunk"
+import usersReducers from "./reducers/usersReducers";
 
 
 
-const store=createStore()
+const store=createStore(usersReducers,applyMiddleware(thunk))
+
+export default store
