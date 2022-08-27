82752295ac5921affb1ab562c4dc3faaffe9aa53
kurvahari <harikrishna62815755@gmail.com>
2022-08-27 14:47:45 +0530

    created-reducers

---
+++ src/redux/reducers/usersReducers.js
@@ -0,0 +1,32 @@
+import { type } from "@testing-library/user-event/dist/type";
+import { createRenderer } from "react-dom/test-utils";
+
+const initialState={
+    users:[],
+    isLoading,
+    error
+}
+
+export default usersReducers=createRenderer(state=initialState,action){
+    switch(action.type){
+        case type.FETCH_USER_START:
+            return{
+                ...state,
+                isLoading:true
+            }
+        case type.FETCH_USER_SUCCESS:
+            return{
+                users:action.payload,
+                isLoading:false,
+                ...state
+            }
+        case type.FETCH_FAILUR:
+            return{
+                ...state,
+                isLoading:false,
+                error:action.payload
+            }
+        default:
+            return state
+    }
+}
