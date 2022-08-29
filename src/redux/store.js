e13353dd3eb4d9fd40020d3ef92f33b02cd7f077
kurvahari <harikrishna62815755@gmail.com>
2022-08-29 10:26:58 +0530

    added-redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,21 @@ import { createStore,applyMiddleware } from "redux";
 import thunk from "redux-thunk"
 import usersReducers from "./reducers/usersReducers";
 import { composeWithDevTools } from "redux-devtools-extension";
-import {logger} from "redux-logger";
+import logger from "redux-logger";
+import { persistStore,persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 
+const persistConfig={
+    key:"persist-users",
+    storage
+}
 
+const persistreducer=persistReducer(persistConfig,usersReducers)
 
-const store=createStore(usersReducers,composeWithDevTools(applyMiddleware(logger,thunk)))
+const store=createStore(persistreducer,composeWithDevTools(applyMiddleware(logger,thunk)))
+const persistor=persistStore(store)
+
+export {persistor}
 
 export default store
